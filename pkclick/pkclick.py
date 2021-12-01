22d18331c920190f38ddf1bd59ab3ad0d9d86e83
Carsten Friis Rundsten <carsten.rundsten@gmail.com>
2021-12-01 14:19:53 +0100

    v1.1.2 Hunting the "'utf-8' codec can't decode byte" problem

--- pkclick/pkclick.py
+++ pkclick/pkclick.py
@@ -25,7 +25,7 @@ logger = logging.getLogger(__name__)
 
 def unicodeerror_handler(exc): 
 	logger.warning(f"Current codec '{exc.encoding}' can't decode byte {exc.object[exc.start:exc.end]} found in input. Byte will be replaced with '?'.")
-	logger.info(exc.object[exc.end:])
+	logger.info(f"{exc.object[0:exc.start]}?{exc.object[exc.end:]}")
 	return (f"{exc.object[0:exc.start]}?{exc.object[exc.end:]}", exc.end)
 #	return (u"[bad char]", exc.end)
 codecs.register_error("UnicodeError", unicodeerror_handler)
