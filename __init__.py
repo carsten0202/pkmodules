3dd5880df934062a224ef0a826b3cf19e0a4684a
Carsten Rundsten <carsten.rundsten@sund.ku.dk>
2024-09-04 14:44:12 +0200

    Changed it so not everything is imported all the time

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 
-from .pkclick import *
-from .pkcsv import *
-from .pkmath import *
-from .pksnp import *
+# Probably shouldn't import everything here. pklib has gotten big and I won't need all of it in the same script.
+#from .pkclick import *
+#from .pkcsv import *
+#from .pkmath import *
+#from .pksnp import *
 
